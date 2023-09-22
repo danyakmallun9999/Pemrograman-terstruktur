8c249c9b1d759544c6544468c6cb77e3f9ff0351
danyakmallun9999 <danyakmallun9999@gmail.com>
2023-09-22 22:04:48 +0700

    Menambahkan beberapa contoh tipe data yang ada di python

--- tipe_data.py
+++ tipe_data.py
@@ -15,6 +15,23 @@ print('Nilai = ', integer, 'Tipe datanya = ', type(integer))
 tipe_data_float = 20.1
 print('Nilai = ', tipe_data_float, 'Tipe datanya = ', type(tipe_data_float))
 
-# Tipe data float = angka dengan koma
-tipe_data_float = 20.1
-print('Nilai = ', tipe_data_float, 'Tipe datanya = ', type(tipe_data_float))
+# Tipe data string = Kumpulan sebuah karakter
+tipe_data_string = "Sella"
+print('Nilai = ', tipe_data_string, 'Tipe datanya = ', type(tipe_data_string))
+
+# Tipe data bool = true / false
+tipe_data_boolean = True
+print('Nilai = ', tipe_data_boolean, 'Tipe datanya = ', type(tipe_data_boolean))
+
+# Bilangan kompleks atau khsusus
+# biasa digunakan sama anak matematika
+
+# bilangan kompleks
+tipe_data_kompleks = complex(5,6)
+print('Nilai = ', tipe_data_kompleks, 'Tipe datanya = ', type(tipe_data_kompleks))
+
+# tipe data dari bahasa c
+from ctypes import c_double, c_bool
+
+data_c_double = c_double(10.8)
+print('Nilai = ', data_c_double, 'Tipe datanya = ', type(data_c_double))
