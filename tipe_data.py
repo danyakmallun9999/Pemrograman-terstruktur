c519092c355c1e068e151e583edb923aeec3f2ee
danyakmallun9999 <danyakmallun9999@gmail.com>
2023-09-22 21:47:47 +0700

    Menambahkan readme, tipe_data dan variable

---
+++ tipe_data.py
@@ -0,0 +1,20 @@
+# Tipe data
+# Ada 6 tipe data di python
+# 1). Integer (int)
+# 2). Float(float)
+# 3). String(str)
+# 4). Boolean(bool)
+# 5). Complex(complex)
+# 6). Ctypes(diambil dari bahasa C)
+
+# Tipe data integer ( int )
+integer = 20
+print('Nilai = ', integer, 'Tipe datanya = ', type(integer))
+
+# Tipe data float = angka dengan koma
+tipe_data_float = 20.1
+print('Nilai = ', tipe_data_float, 'Tipe datanya = ', type(tipe_data_float))
+
+# Tipe data float = angka dengan koma
+tipe_data_float = 20.1
+print('Nilai = ', tipe_data_float, 'Tipe datanya = ', type(tipe_data_float))
---
+++ variable.py
@@ -0,0 +1,23 @@
+# Ini adalah latihan logika pemrograman menggunakan bahasa pemrograman python
+'''
+Belajar tentang sejarah dan awal mula perjalanan bahasa python
+
+Nama Pembuat : Guido van rossum
+Tanggal : 1989
+
+'''
+
+# Variable = Variable di matematika 
+
+x = 10
+y = x + 20
+
+z = x + y # z = 40
+print(z)
+
+x = 5
+print(x)
+
+
+
+
