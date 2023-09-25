a3f6e22254c59dc6b20488ba5e581377369798db
danyakmallun9999 <danyakmallun9999@gmail.com>
2023-09-25 23:10:45 +0700

    menambahkan file input.py

---
+++ input.py
@@ -0,0 +1,7 @@
+# Sinau inputan ing python
+
+# contoh mengambil sebuah inputan umur
+umur = input('Masukkan umur anda = ') # reminder = input akan menghasilkan nilai string
+
+# mencetak variable umur
+print('Umur anda adalah = ', umur, 'Tahun') 
