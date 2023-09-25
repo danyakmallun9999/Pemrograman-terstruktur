c8e7fcf60399eb3480243984cff2980868d2556f
danyakmallun9999 <danyakmallun9999@gmail.com>
2023-09-25 21:52:47 +0700

    menambahkan contoh konversi dari str ke int

---
+++ casting.py
@@ -0,0 +1,6 @@
+# Pyhton dapat mengonversi (casting) tipe data dari satu tipe ke tipe lainnya di Python. Ini berguna ketika perlu mengubah tipe data untuk melakukan operasi tertentu atau ketika ingin memastikan bahwa data yang gunakan sesuai dengan tipe yang diharapkan. Berikut adalah beberapa contoh bagaimana dapat melakukan konversi tipe data di Python:
+
+# Konversi ke INT (integer)
+angka = "32"
+angka_integer = int(angka)
+print('String di ubah ke integer = ', type(angka_integer))
