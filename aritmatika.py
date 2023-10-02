eb85c2b17b243f78b7b2771a236070726bdd98e8
danyakmallun9999 <danyakmallun9999@gmail.com>
2023-10-02 22:26:57 +0700

    Menambahkan baris kode di dalam file aritmatika

--- aritmatika.py
+++ aritmatika.py
@@ -1,2 +1,34 @@
 # Belajar operasi aritmatika di pyhton
+# Operasi aritmatika
 
+# variable nilai
+nilai_1 = 29
+nilai_2 = 7
+
+# Operasi penjumlahan pada python
+hasil_nilai = nilai_1 + nilai_2
+print(nilai_1, '+' , nilai_2, '=', hasil_nilai)
+
+# Operasi pengurangan pada python
+hasil_nilai = nilai_1 - nilai_2
+print(nilai_1, '-' , nilai_2, '=', hasil_nilai)
+
+# Operasi perkalian pada python
+hasil_nilai = nilai_1 * nilai_2
+print(nilai_1, '*' , nilai_2, '=', hasil_nilai)
+
+# Operasi pembagian pada python
+hasil_nilai = nilai_1 / nilai_2
+print(nilai_1, '/' , nilai_2, '=', hasil_nilai)
+
+# Operasi sisa bagi pada python
+hasil_nilai = nilai_1 % nilai_2
+print(nilai_1, '%' , nilai_2, '=', hasil_nilai)
+
+# Operasi perpangkatan pada python
+hasil_nilai = nilai_1 ** nilai_2
+print(nilai_1, '**' , nilai_2, '=', hasil_nilai)
+
+# Operasi floor division pada python
+hasil_nilai = nilai_1 // nilai_2
+print(nilai_1, '/' , nilai_2, '=', hasil_nilai)
