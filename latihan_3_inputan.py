5deacd15d13f56a1c1aec6dcc345e48a4d052176
danyakmallun9999 <danyakmallun9999@gmail.com>
2023-10-12 11:42:51 +0700

    Menambahkan file latihan 3 dan merubah nama file latihan 5

---
+++ latihan_3_inputan.py
@@ -0,0 +1,12 @@
+print ("    *********** BIODATA ***********")
+print (" ")
+
+nama            = ("Dany Akmallun Niam")
+tempat_lahir    = ("Jepara")
+tanggal_lahir   = ("03 Oktober 2003")
+alamat          = ("Bendowangin, Mayonglor Jepara")
+
+print ("Nama Saya adalah     : ", nama)
+print ("Saya Lahir di        : ", tempat_lahir)
+print ("Tanggal lahir saya   : ", tanggal_lahir)
+print ("Alamat saya          : ", alamat)
