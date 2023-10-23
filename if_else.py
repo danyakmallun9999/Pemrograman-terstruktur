206ff9d983f7f5cd322258cde2ad1caff0c287af
danyakmallun9999 <danyakmalln9999@gmail.com>
2023-10-23 08:23:09 +0700

    Mengubah beberapa dokumen

--- if_else.py
+++ if_else.py
@@ -1,8 +1,8 @@
 # Membuat hasil nilai mahasiswa
 # Jika nilai > 90 dapat A, Jika > 80 dapat B, Jika > 70 dapat c, Selain itu dapat D
 
-nama_mahasiswa = input("Masukkan namamu : ")
-nilai_mahasiswa = int(input("Masukkan nilai : "))
+nama_mahasiswa =  input("Masukkan namamu : ")
+nilai_mahasiswa = int(input("Masukkan nilai  : "))
 
 print(" ")
 print(" ")
@@ -10,10 +10,10 @@ print(" ")
 print("Nama :", nama_mahasiswa)
 
 if nilai_mahasiswa >= 90 :
-    print("Kamu mendapatkan Nilai A")
+    print("Kamu mendapatkan Nilai : A")
 elif nilai_mahasiswa >= 80 :
-    print("Kamu mendapatkan Nilai B")
+    print("Kamu mendapatkan Nilai : B")
 elif nilai_mahasiswa >= 70 :
-    print("Kamu mendapatkan nilai C")
+    print("Kamu mendapatkan nilai : C")
 else :
-    print("Kamu mendapatkan Niali D")
+    print("Kamu mendapatkan Niali : D")
