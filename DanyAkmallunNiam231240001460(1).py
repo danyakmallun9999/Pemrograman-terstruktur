972d31629b8e6971d256873fe385c338d9815f4f
danyakmallun9999 <danyakmalln9999@gmail.com>
2023-10-24 10:23:21 +0700

    Menambahkan file tugas python

---
+++ DanyAkmallunNiam231240001460(1).py
@@ -0,0 +1,37 @@
+print("*************************************************************************")
+print("****************** Program Menghitung Nilai Tugas ***********************")
+print("*************************************************************************")
+print(" ")
+print(" ")
+
+nama_mahasiswa    = input("Masukkan Nama Mahasiswa    : ")
+nim_mahasiswa     = input("Masukkan Nim Mahasiswa     : ")
+mata_Kuliah       = input("Masukkan Mata Kuliah       : ")
+nilai_tugas_satu  = input("Masukkan Nilai Tugas 1     : ")
+nilai_tugas_dua   = input("Masukkan Nilai Tugas 2     : ")
+nilai_tugas_tiga  = input("Masukkan Nilai Tugas 3     : ")
+
+print(" ")
+print(" ")
+print("-------------------------------------------------------------------------")
+print(" ")
+print(" ")
+
+# Kalkulasi nilai
+# rumus = ( nilai1 + nilai2 + nilai3 ) / 3 lalu dibulatkan kebawah
+total_nilai = (int(nilai_tugas_satu) + int(nilai_tugas_dua) + int(nilai_tugas_tiga)) / 3
+
+print("Nilai Tugas")
+print("Nama Mahasiswa               :", nama_mahasiswa)
+print("Nama Nim Mahasiswa           :", nim_mahasiswa)
+print("Mata Kuliah                  :", mata_Kuliah)
+print("Total nilai adalah           :", total_nilai)
+print("Nilai dibulatkan menjadi     :", round(total_nilai))
+
+print(" ")
+print(" ")
+print("-------------------------------------------------------------------------")
+print(" ")
+print(" ")
+
+print("******************** Tingkatkan Lagi Belajarnya *************************")
