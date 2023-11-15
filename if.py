3d3c10d23d5e87f6d2055731a6c9cd66648a00c0
danyakmallun9999 <danyakmallun9999@gmail.com>
2023-11-15 23:25:30 +0700

    Menambahkan file latihan if.py

---
+++ if.py
@@ -0,0 +1,38 @@
+# umur = 18
+
+# if umur < 18:
+#     print('Masih Bocillll')
+# else:
+#     print('Oke, kamu sudah dewasa!')
+
+
+
+
+# Nilai = 100
+
+# if Nilai >= 90:
+#     print('Nilai kamu A')
+# elif Nilai >= 80:
+#     print('Nilai kamu B')
+# elif Nilai >= 70:
+#     print('Nilai Kamu C')
+# elif Nilai >= 60:
+#     print('Nilai Kamu D')
+# else :
+#     print('Kamu harus giat belajar!')
+
+Nilai_uts = 80
+Nilai_uas = 90
+
+if Nilai_uts >= 95 or Nilai_uas >= 95:
+    print('Kamu pasti rajin belajar')
+elif Nilai_uts >= 90 or Nilai_uas >= 90:
+    print('Nilai kamu A')
+elif Nilai_uts >= 80 or Nilai_uas >= 80:
+    print('Nilai kamu B')
+elif Nilai_uts >= 70 or Nilai_uas >= 70:
+    print('Nilai Kamu C')
+elif Nilai_uts >= 60 or Nilai_uas >= 60:
+    print('Nilai Kamu D')
+else :
+    print('Kamu harus giat belajar!')
