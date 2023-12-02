6198346862cdf99f0251741d3f98b51a05395a2f
Dany Akmallun Ni'am <88391128+danyakmallun9999@users.noreply.github.com>
2023-12-02 23:13:40 +0700

    Menambahkan file for

--- for.py
+++ for.py
@@ -1,13 +1,28 @@
-for i in range(10):
-    print('Mahasiswa ke -',i+1)
+# pengulangan sebanyak 5 kali
 
-i = 1
-Mahasiswa = 34
-while i <= Mahasiswa:
-    if i == 2:
-        print('Mahasiswa Ke - ',i , 'Rajin')
-    print('Mahasiswa Ke - ', i)
-    i += 1
+for mhs in [1,2,3,4,5]:
+    print("Universitas Islam Nahdlatul Ulama Jepara")
+    
+print(" ")
 
+for fklts in [1,2,3]:
+    print("Fakultas Sains dan Teknolgi")
+    
+print(" ")
 
+for jrsn in [1,2]:
+    print("Program Studi Teknik Informatika")
+    
+print(" ")
 
+angka = ['satu','dua','tiga','empat','lima','enam']
+for a in angka:
+    print('Dia adalah anggota HIPMI ke :',a)
+
+print(" ")
+
+sayur = ['sayur bening', 'horok-horok', 'Janganan', 'Tempe godok','Oseng tahu', 'Tempe orek']
+for syr in sayur:
+    print(syr, "Adalah Masakan khas dari daerah Jepara")
+
+print(" ")
---
+++ range.py
@@ -0,0 +1,27 @@
+print (list(range(1,10)))
+
+print (list(range(1,10,2)))
+print (list(range(1,10,3)))
+print (list(range(1,10,4)))
+print (list(range(1,10,5)))
+print (list(range(1,10,6)))
+
+print (list(range(1,10,11)))
+
+print (list(range(10,1)))
+
+print (list(range(10,1,2)))
+print (list(range(10,1,11)))
+
+print (list(range(10,1,-1)))
+print (list(range(10,1,-11)))
+
+#print (list(range(1,10,0)))
+#print (list(range(10,1,0)))
+
+for i in range (20):
+    print("Ini Pengulangan ke - ",i)
+
+
+for i in range (1,10,2):
+    print("Nilai rata-rata nilai kelas",i,"adalah : 85")
