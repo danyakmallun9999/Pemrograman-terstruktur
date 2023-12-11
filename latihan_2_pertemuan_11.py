b489317fac2456f96ce61d49fb358b4904012443
danyakmallun9999 <danyakmallun9999@gmail.com>
2023-12-12 05:45:25 +0700

    Merubah extensi file

---
+++ latihan_2_pertemuan_11.py
@@ -0,0 +1,8 @@
+# Dany Akmallun Ni'am
+# Teknik Informatika Kelas DC;
+
+kalimat_dicetak        =     input("Masukkan Kalimat yang ingin di cetak : ")
+jumlah_kalimat_dicetak = int(input("Masukkan berapa kali pencetakan      : "))
+
+for i in range(jumlah_kalimat_dicetak):
+    print(kalimat_dicetak)
